2bd241de1b0ae661f22588840ae2f059efeeaba2
Federico Bo <federico.bo@hcl.com>
2020-09-15 10:16:52 +0200

    first commit jsx/seasons

--- components/src/index.js
+++ components/src/index.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                    </div></ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:45PM"
+                    comment="Third Comment!"
+                    imgSrc={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 2:00AM"
+                    comment="Second Comment!"
+                    imgSrc={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Yesterday at 5:00PM"
+                    comment="First comment!"
+                    imgSrc={faker.image.avatar()} />
+            </ApprovalCard>
+
+        </div>
+    )
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+
+
+//https://semantic-ui.com/
+//https://semantic-ui cdn
